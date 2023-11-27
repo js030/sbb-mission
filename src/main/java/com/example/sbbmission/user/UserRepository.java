92c105c53b24b687e45604ad20604c9a986b189e
kyumho kim <rlaruagh030@gmail.com>
2023-11-27 22:01:46 +0900

    feat : sbb 3-07 완료

--- src/main/java/com/example/sbbmission/user/UserRepository.java
+++ src/main/java/com/example/sbbmission/user/UserRepository.java
@@ -1,6 +1,8 @@
 package com.example.sbbmission.user;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByUsername(String username);
 }
