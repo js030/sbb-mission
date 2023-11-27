c1b4f3d536b5cafbcf195c9dd5bc08f07b5d074f
kyumho kim <rlaruagh030@gmail.com>
2023-11-27 20:17:24 +0900

    feat : sbb 2-15 완료

--- src/main/java/com/example/sbbmission/question/QuestionService.java
+++ src/main/java/com/example/sbbmission/question/QuestionService.java
@@ -1,6 +1,7 @@
 package com.example.sbbmission.question;
 
 import com.example.sbbmission.DataNotFoundException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -23,5 +24,13 @@ public class QuestionService {
         }else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }
