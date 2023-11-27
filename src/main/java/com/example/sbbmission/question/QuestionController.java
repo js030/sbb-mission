70e378913edce59ec725ab6ebd8bc5b3017caf32
kyumho kim <rlaruagh030@gmail.com>
2023-11-27 18:46:45 +0900

    feat : sbb 2-10 완료

--- src/main/java/com/example/sbbmission/question/QuestionController.java
+++ src/main/java/com/example/sbbmission/question/QuestionController.java
@@ -1,25 +1,32 @@
-package com.example.sbbmission.controller;
+package com.example.sbbmission.question;
 
-import com.example.sbbmission.question.Question;
-import com.example.sbbmission.question.QuestionRepository;
-import com.example.sbbmission.question.QuestionService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model){
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/example/sbbmission/question/QuestionService.java
+++ src/main/java/com/example/sbbmission/question/QuestionService.java
@@ -1,6 +1,8 @@
 package com.example.sbbmission.question;
 
+import com.example.sbbmission.DataNotFoundException;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +14,14 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()){
+            return question.get();
+        }else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
