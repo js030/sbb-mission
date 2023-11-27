80527a881d24a265267aa3f51e1bdad07ca4d22d
kyumho kim <rlaruagh030@gmail.com>
2023-11-27 19:30:44 +0900

    feat : sbb 2-11 완료

---
+++ src/main/java/com/example/sbbmission/answer/AnswerService.java
@@ -0,0 +1,21 @@
+package com.example.sbbmission.answer;
+
+import com.example.sbbmission.question.Question;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content){
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
